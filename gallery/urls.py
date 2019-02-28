725463c0eee372817168feae6497b49b535ea076
KingVulkan <manowfelow@gmail.com>
2019-02-28 10:30:10 +0300

    removes ^ in urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static 
 
 urlpatterns = [
-    url(r'^$',views.gallerys_today,name='gallerysToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallerys,name = 'pastGallerys'),
+    url(r'^$',views.gallery_today,name='galleryToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^category/$',views.category_image,name='categorysToday'),
     url(r'^location/(\d+)',views.location,name='location'),
